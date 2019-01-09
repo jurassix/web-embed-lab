6d5dccf430b646a46ac843a5507152d427c8863f
Trevor F. Smith <trevor@trevor.smith.name>
2019-01-09 14:55:11 -0800

    Now loading templates based on routes in the page formula.

--- go/src/wel/services/host/formulas.go
+++ go/src/wel/services/host/formulas.go
@@ -2,10 +2,12 @@ package host
 
 import (
 	"errors"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+	"regexp"
 
 	"wel/formulas"
 )
@@ -36,6 +38,8 @@ func NewFormulaHost(formulasPath string) (*FormulaHost, error) {
 		PageFormulas:   map[string]*formulas.PageFormula{},
 	}
 
+	var firstName = ""
+
 	for _, file := range files {
 		if file.IsDir() == false {
 			continue
@@ -55,20 +59,72 @@ func NewFormulaHost(formulasPath string) (*FormulaHost, error) {
 			logger.Printf("Could not open %v", infoPath)
 			continue
 		}
+		defer func() {
+			infoFile.Close()
+		}()
 		pageFormula, err := formulas.ParsePageFormulaInfo(infoFile)
 		if err != nil {
 			logger.Printf("Could not parse %v: %v", file.Name(), err)
 			continue
 		}
 		host.PageFormulas[file.Name()] = pageFormula
+		if firstName == "" {
+			firstName = file.Name()
+		}
 	}
 
-	logger.Println("Page formulas", host.PageFormulas)
+	if len(host.PageFormulas) == 0 {
+		return nil, errors.New("Read no page formulas")
+	}
+	host.CurrentFormula = firstName
 
 	return host, nil
 }
 
+func (host *FormulaHost) MatchRoute(path string) *formulas.Route {
+	if host.CurrentFormula == "" {
+		return nil
+	}
+	for _, route := range host.PageFormulas[host.CurrentFormula].Routes {
+		matched, err := regexp.MatchString(route.Path, path)
+		if err != nil {
+			logger.Printf("Error in regexp \"%v\": %v", route.Path, err)
+			return nil
+		}
+		if matched {
+			return &route
+		}
+	}
+	return nil
+}
+
 func (host *FormulaHost) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	// TODO
-	writer.Write([]byte("Hi there"))
+	route := host.MatchRoute(request.URL.Path)
+	if route == nil {
+		writer.WriteHeader(http.StatusNotFound)
+		writer.Write([]byte("No route"))
+		return
+	}
+	switch route.Type {
+	case formulas.TemplateRoute:
+		host.handleTemplateRoute(route, writer, request)
+	default:
+		logger.Println("Unknown route", route.Type)
+		writer.WriteHeader(http.StatusNotFound)
+		writer.Write([]byte("Unknown route"))
+	}
+}
+
+func (host *FormulaHost) handleTemplateRoute(route *formulas.Route, writer http.ResponseWriter, request *http.Request) {
+	routeTemplate, err := template.ParseFiles(path.Join(host.FormulasPath, host.CurrentFormula, "template", route.Value))
+	if err != nil {
+		logger.Println("Template error", err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("Template error"))
+		return
+	}
+	err = routeTemplate.Execute(writer, route.Parameters)
+	if err != nil {
+		logger.Println("Template execution error", err)
+	}
 }

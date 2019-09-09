7f882d103811ddb1b629da9b22b96653a2d79167
Trevor F. Smith <trevor@trevor.smith.name>
2019-09-09 10:43:22 -0700

    Add a handy script (really just a curl command) to change the page formula hosted by the runner command.

---
+++ scripts/change-formula.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Changes the runner's page formula 
+# usage: ./scripts/change-formula.sh {formula-name}
+
+curl http://127.0.0.1:9090/__wel_control -X PUT --data "{\"current-formula\":\"$1\"}"
